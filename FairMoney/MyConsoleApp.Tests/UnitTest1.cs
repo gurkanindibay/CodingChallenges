7c25bab46eee692b149da14fea5352ff574173b4
Gurkan Indibay <gurkanindibay@gmail.com>
2025-07-21 20:31:36 +0300

    Initial project structure

---
+++ FairMoney/MyConsoleApp.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class ProgramTests
+{
+    [Test]
+    public void HelloWorldTest()
+    {
+        Assert.That(Program.HelloWorld(), Is.EqualTo("Hello, World!"));
+    }
+}
---
+++ FairMoney/MyConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Program
+{
+    // Main method for console execution - rename to avoid conflicts during testing
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(HelloWorld());
+    }
+
+    public static string HelloWorld()
+    {
+        return "Hello, World!";
+    }
+}
